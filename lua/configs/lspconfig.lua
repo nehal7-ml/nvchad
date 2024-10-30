d3c06434f87b7e6391c424d92d41d50c803e3ec4
nehal7-ml <nehal.sk.99@gmail.com>
2024-10-31 03:24:38 +0530

    chore: lsp, supermaven added

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls" }
+local servers = { "html","ast_grep", "cssls", "lua_ls","vtsls","tailwindcss", "pylyzer" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
